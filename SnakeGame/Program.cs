553079c175464fe3263079de8796112389147caa
surajbridgelabz <surajnsinha@gmail.com>
2022-08-24 18:52:27 +0530

    [suraj] Add: Welcome to snake ladder game

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Game problem statement");
+        }
+    }
+}
